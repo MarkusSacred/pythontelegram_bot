faf51c8ff6a9420204008bdd720f407d96c6aa0e
MarkusSacred <markarcedas27@gmail.com>
2025-01-02 10:16:12 +0800

    connection

---
+++ checkConneciton.py
@@ -0,0 +1,23 @@
+from typing import Final
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+TOKEN : Final = "8038816092:AAH4P4-KS33BU8hVAQwpjkGbFka49o_QN0M"
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
